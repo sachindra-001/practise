a29e39a34853ba6ed8a6ae87f5887ac331385cf9
sachindra-001 <gr5930404@gmail.com>
2026-02-19 23:00:30 +0530

    Initial commit: notes app

---
+++ PART2/vite-project/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  // 1. Fetch data from the backend
+  useEffect(() => {
+    // In Part 3, we use a relative URL if the frontend is served by the backend
+    // Or http://localhost:3001/api/notes during development
+    axios.get("/api/notes").then((response) => {
+      setNotes(response.data);
+    });
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    };
+
+    // 2. Send new note to the Express backend
+    axios.post("/api/notes", noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    });
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
