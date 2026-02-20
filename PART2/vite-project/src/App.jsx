1fc8d694637603f9aa0304e530849d51fee9b253
sachindra <your-email@example.com>
2026-02-20 21:41:27 +0530

    complete

--- PART2/vite-project/src/App.jsx
+++ PART2/vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Note from "./components/Note";
+import Note from "./components/note";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
--- part3/index.js
+++ part3/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
-
+app.use(express.static("dist"));
 let notes = [
   {
     id: "1",
