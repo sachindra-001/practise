4db9009207ec58c0c2e833dab678f63d61005750
sachindra <your-email@example.com>
2026-02-27 18:34:05 +0530

    completed exercise 22.2

--- part3/eslint.config.mjs
+++ part3/eslint.config.mjs
@@ -1,8 +1,8 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import stylisticJs from "@stylistic/eslint-plugin";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import stylisticJs from '@stylistic/eslint-plugin'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -15,9 +15,9 @@ export default [
 
   // 3. Your Custom Configuration
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     plugins: {
-      "@stylistic/js": stylisticJs,
+      '@stylistic/js': stylisticJs,
     },
     languageOptions: {
       globals: {
@@ -26,28 +26,28 @@ export default [
       },
     },
     rules: {
-      "@stylistic/js/indent": ["error", 2],
-      "@stylistic/js/linebreak-style": ["error", "unix"],
-      "@stylistic/js/quotes": ["error", "single"],
-      "@stylistic/js/semi": ["error", "never"],
-      "no-unused-vars": "warn", // 1. Allow require() statements
-      "@typescript-eslint/no-require-imports": "off",
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      'no-unused-vars': 'warn', // 1. Allow require() statements
+      '@typescript-eslint/no-require-imports': 'off',
 
       // 2. Turn off the "unused vars" error (or set it to "warn")
-      "no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
+
+      '@typescript-eslint/no-unused-vars': 'warn',
 
       // 3. Allow console.log() statements
-      "no-console": "off",
-      eqeqeq: "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": ["error", "always"],
-      "arrow-spacing": ["error", { before: true, after: true }],
+      'no-console': 'off',
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
     },
   },
 
   // 4. Global Ignores
   {
-    ignores: ["dist/**"],
+    ignores: ['dist/**'],
   },
-];
+]
