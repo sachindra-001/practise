24eed0b1a79134d333a600e0b4613b2744b07873
sachindra-001 <gr5930404@gmail.com>
2026-03-08 14:22:28 +0530

    staring exercises

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,4 @@
-const app = require('./app') // the actual Express application
+const app = require('./app') // Imports the app from app.js
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
